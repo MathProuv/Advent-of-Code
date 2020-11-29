a6539503474c9ab09e1102f8af629919201c28c1
MathProuv <MathProuv@users.noreply.github.com>
2020-11-29 23:28:00 +0100

    partie 2

--- day5.py
+++ day5.py
@@ -2,7 +2,7 @@ file = open("day5.txt", "r")
 
 compt = 0
 
-def check(s):
+def check1(s):
 	if "ab" in s or "cd" in s or "pq" in s or "xy" in s:
 		return False
 	if s.count("a") + s.count("e") + s.count("i") + s.count("o") + s.count("u") < 3 :
@@ -11,6 +11,15 @@ def check(s):
 		if s[i] == s[i-1]:
 			return True
 	return False
+
+def check(s):
+	paire, sandwitch = False, False
+	for i in range(len(s)-2):
+		if not sandwitch and s[i] == s[i+2]:
+			sandwitch = True
+		if not paire and s[i:i+2] in s[i+2:]:
+			paire = True
+	return sandwitch and paire
 
 for line in file:
 	compt += check(line)
