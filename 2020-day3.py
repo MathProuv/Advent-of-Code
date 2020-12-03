e16557b3f331f24a3cce1433f460aca5742bee4d
MathProuv <MathProuv@users.noreply.github.com>
2020-12-03 08:01:38 +0100

    modified:   2020-day3.py

--- 2020-day3.py
+++ 2020-day3.py
@@ -6,18 +6,19 @@ height,width = len(input),len(input[0])
 res = 0
 
 for i in range(height):
-    res += input[i][(3*i)%width] == "#"
+    res += input[i][3*i%width] == "#"
 
 print(res)
 
-ratios = ((1,1), (1,5), (1,7), (2,1))
-#res = res # res(ratio=(1,3))
+
+ratios = ((1,1), (1,5), (1,7), (2,1)) #, (1,3))
+#res = 1
 
 for ratio in ratios:
     res_ratio = 0
-    for lig in range(0, height, ratio[0]):
-        col = lig // ratio[0]
-        res_ratio += input[lig][(ratio[1]*col)%width] == "#"
+    for lig in range(ratio[0], height, ratio[0]):
+        col = ratio[1] * lig // ratio[0]
+        res_ratio += input[lig][col % width] == "#"
     #print(res_ratio)
     res *= res_ratio
 
