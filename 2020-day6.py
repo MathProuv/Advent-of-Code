8d48d305780eec540bb0e83bb0ddacb7e33be274
MathProuv <MathProuv@users.noreply.github.com>
2020-12-06 11:58:02 +0100

    day 6

---
+++ 2020-day6.py
@@ -0,0 +1,22 @@
+import inputAoC as aoc
+groups = aoc.get_input_file(6).split("\n\n")
+
+res1 = 0
+for group in groups:
+    x = group.replace("\n","")
+    letters = set()
+    for letter in x:
+        letters.add(letter)
+    res1 += len(letters)
+
+print(res1)
+
+res2 = 0
+for group in groups:
+    x = group.split("\n")
+    letters = set(x[0])
+    for word in x[1:]:
+        letters = letters.intersection(set(word))
+    res2 += len(letters)
+
+print(res2)
