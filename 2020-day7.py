e08ef7a0c8d04ca2fe79012576891711c455e002
MathProuv <MathProuv@users.noreply.github.com>
2020-12-07 19:28:32 +0100

    day 7 mieux rédigé

--- 2020-day7.py
+++ 2020-day7.py
@@ -8,9 +8,9 @@ contains = []
 
 for rule in rules:
     index_bag = rule.index(" bags ")
-    index_contain = index_bag + len(" bags contain ")
     bag = rule[:index_bag]
     bags.append(bag)
+    index_contain = index_bag + len(" bags contain ")
     contain = rule[index_contain:].replace(" bag, ", "\n").replace(" bags, ","\n").replace(" bag.","").replace(" bags.","").split("\n")
     if contain[0] == "no other":
         contain = []
@@ -22,14 +22,15 @@ for rule in rules:
     contains.append(contain)
 
 def see_is_in_bag(to_see,seen):
+    """Fonction récursive pour compter combien de sacs peuvent contenir to_see"""
     seen_here = set()
     if not to_see:
         return len(seen)
-    for i in range(len(bags)):
-        containing = contains[i]
+    for bag in bags:
+        containing = contains[bags.index(bag)]
         for n,b in containing:
             if b in to_see:
-                seen_here.add(bags[i])
+                seen_here.add(bag)
     seen |= seen_here
     to_see = list(seen_here)
     return see_is_in_bag(to_see, seen)
@@ -38,6 +39,7 @@ res1 = see_is_in_bag(["shiny gold"], set())
 print(res1)
 
 def see_in_bag(bag):
+    """Fonction récursive pour compter combien de sacs contient bag"""
     if not bag:
         return 0
     res = 0
