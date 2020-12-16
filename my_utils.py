fcbbaba66b61782857ce541cb6da6708179ea591
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2020-12-16 21:19:41 +0100

    documented my_utils with types

--- my_utils.py
+++ my_utils.py
@@ -1,21 +1,21 @@
-def print_dict(dict):
-    for key, value in dict.items():
+def print_dict(dictionaire: dict) -> None:
+    for key, value in dictionaire.items():
         print(key, ":", value)
     print()
 
-def print_matrice(matrice):
-    for line in matrice:
+def print_list(liste: list) -> None:
+    for line in liste:
         print(line)
     print()
 
-def rindex(liste, elem):
+def rindex(liste: list, elem) -> int:
     if elem not in liste:
         return -1
     for i in range(len(liste)-1,-1,-1):
         if liste[i] == elem:
             return i
 
-def bin_n(number, n):
+def bin_n(number: int, n: int) -> str:
     """Renvoie la string de number en binaire de taille n"""
     res = bin(number)[2:]
     res = '0'*(n-len(res)) + res
