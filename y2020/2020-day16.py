de6bd527b8c9a6cb115f4e3d7d63a2f46068cfc5
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2020-12-16 15:53:30 +0100

    documented day16

--- y2020/2020-day16.py
+++ y2020/2020-day16.py
@@ -2,10 +2,10 @@ import inputAoC as aoc
 import re
 import my_utils
 
-def constr_fields(groups_instrs) -> dict:
+def constr_fields(instrs: str) -> dict:
     fields = dict()
 
-    for field in groups_instrs[0].splitlines():
+    for field in instrs:
         m = re.match(r"([a-z ]+): (\d+)-(\d+) or (\d+)-(\d+)", field)
         f, a1, b1, a2, b2 = m.groups()
         fields[f] = [(int(a1),int(b1)), (int(a2),int(b2))]
@@ -13,43 +13,42 @@ def constr_fields(groups_instrs) -> dict:
     #my_utils.print_dict(fields)
     return fields
 
-def fields_ranges(fields:dict) -> [tuple]:
+def fields_ranges(fields: dict) -> [tuple]:
     ranges = []
     for v_field in fields.values():
         ranges.extend(v_field)
     return ranges
 
-def ticket_score(ticket:str, fields:dict):
+def ticket_score(ticket: [int], fields: dict) -> int:
     ranges = fields_ranges(fields)
     score = 0
-    for number in ticket.split(","):
-        nb = int(number)
+    for nb in ticket:
         for rang in ranges:
             if rang[0] <= nb <= rang[1]:
                 nb = 0
         score += nb
     return score
 
-def valid_tickets(tickets, fields):
+def get_valid_tickets(tickets: [[int]], fields: dict) -> [[int]]:
     res = []
     for ticket in tickets:
         if not ticket_score(ticket, fields):
-            res.append([int(tick) for tick in ticket.split(',')])
+            res.append(ticket)
     return res
 
-def scan_rate(tickets:[str], fields):
+def scan_rate(tickets: [[int]], fields: dict) -> int:
     rate = 0
     for ticket in tickets:
         rate += ticket_score(ticket, fields)
     return rate
 
 
-def get_possible_field(good_tickets, fields):
-    res = []
+def get_real_fields(valid_tickets: [[int]], fields: dict) -> [str]:
+    ## get_possible_fields
+    possible = []
     for col in range(len(good_tickets[0])):
         values = [line[col] for line in good_tickets]
         possibs = set(fields.keys())
-        #print(values)
         for val in values:
             posses = set()
             for poss in possibs:
@@ -59,13 +58,11 @@ def get_possible_field(good_tickets, fields):
             possibs &= posses
             if len(possibs) <= 1:
                 break
-        res.append(possibs)
-    return res
-#my_utils.print_matrice(possible_fields)
-
-def sudoku(possible):
+        possible.append(possibs)
     possible = list(enumerate(possible))
     # my_utils.print_matrice(possible)
+
+    ## get_real_fields
     res = [str()] * len(possible)
     repeat = True
     while repeat:
@@ -82,23 +79,21 @@ def sudoku(possible):
     return res
 
 
-
 groups_instrs = aoc.get_input_file(16,2020).split("\n\n")
-fields = constr_fields(groups_instrs)
+fields = constr_fields(groups_instrs[0].splitlines())
 my_ticket = [int(nb) for nb in groups_instrs[1].splitlines()[1].split(",")]
 tickets = groups_instrs[2].splitlines()[1:]
+tickets = [[int(nb) for nb in ticket.split(",")] for ticket in tickets]
 
-good_tickets = valid_tickets(tickets, fields)
+good_tickets = get_valid_tickets(tickets, fields)
 # my_utils.print_matrice(good_tickets)
 
 res1 = scan_rate(tickets, fields)
 print(res1)
 
-possible_fields = get_possible_field(good_tickets, fields)
-real_fields = sudoku(possible_fields)
+real_fields = get_real_fields(good_tickets, fields)
 
 res2 = 1
-
 for i, elem in enumerate(real_fields):
     if "departure" in elem:
         res2 *= my_ticket[i]
