ecd836f794063d680b14294a6cee746a9cdb3ae1
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2020-12-19 20:02:04 +0100

    day 19 ex ok mais pas le reste

--- y2020/2020-day19.py
+++ y2020/2020-day19.py
@@ -17,6 +17,7 @@ rules = rules_aoc.splitlines()
 messages = matching_aoc.splitlines()
 
 def concat_2vals(v1, v2):
+    #return [v1,v2]
     if type(v1) == str and type(v2) == str:
         return v1 + v2
     elif type(v1) == tuple and type(v2) == tuple:
@@ -28,15 +29,44 @@ def concat_2vals(v1, v2):
     else:
         print("pb concat", v1, v2)
         return [v1, v2]
+def concat_2vals_set(v1, v2):
+    if type(v1) == str and type(v2) == str:
+        return v1 + v2
+    elif type(v1) == set and type(v2) == set:
+        return set([concat_2vals(v,w) for v in v1 for w in v2])
+    elif type(v1) == set:
+        return set([concat_2vals(v, v2) for v in v1])
+    elif type(v2) == set:
+        return set([concat_2vals(v1, v) for v in v2])
+    else:
+        print("pb concat", v1, v2)
+        return [v1, v2]
+
+
 
 def pip_2vals(v1, v2):
+    #return (v1, v2)
     if type(v1) == str and type(v2) == str:
         return (v1, v2)
+    elif type(v1) == tuple and type(v2) == tuple:
+        return v1 + v2
     elif type(v1) == tuple or type(v2) == tuple:
         return tuple(v1) + tuple(v2)
     else:
         print("pb concat", v1, v2)
         return (v1, v2)
+def pip_2vals_set(v1, v2):
+    if type(v1) == str and type(v2) == str:
+        return set((v1, v2))
+    elif type(v1) == set and type(v2) == set:
+        return v1.union(v2)
+    elif type(v1) == set or type(v2) == set:
+        return set(tuple(v1)).union(tuple(v2))
+    else:
+        print("pb concat", v1, v2)
+        return (v1, v2)
+
+
 
 def get_value(rule, values):
     val = re.match(r"^\"([a-z])\"$", rule)
@@ -45,7 +75,7 @@ def get_value(rule, values):
     pipe = re.match(r"^(.+) \| (.+)$", rule)
     if val:
         return val.groups()[0]
-    if one_rule:
+    elif one_rule:
         r1 = int(one_rule.groups()[0])
         if r1 in values:
             return values[r1]
@@ -53,12 +83,12 @@ def get_value(rule, values):
         r1, r2 = [int(r) for r in rules.groups()]
         if r1 in values and r2 in values:
             v1, v2 = values[r1], values[r2]
-            return concat_2vals(v1, v2)
+            return concat_2vals_set(v1, v2)
     elif pipe:
         rul1, rul2 = pipe.groups()
         v1, v2 = get_value(rul1, values), get_value(rul2, values)
         if v1 and v2:
-            return pip_2vals(v1, v2)
+            return pip_2vals_set(v1, v2)
     else:
         print("no matching")
 
@@ -94,14 +124,19 @@ ex = '''0: 4 1
 3: 4 5 | 5 4
 4: "a"
 5: "b"'''
+messages_ex = '''ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb'''.splitlines()
 
-vals = constr_values(ex.splitlines())
-my_utils.print_dict(vals)
+vals_ex = constr_values(ex.splitlines())
+my_utils.print_dict(vals_ex)
 
-
+print("res ex", count_valids(messages_ex, vals_ex, 6))
 
 vals = constr_values(rules)
-my_utils.print_dict(vals)
+print("nb de possibilités",len(vals[0]))
 
 res1 = count_valids(messages, vals, 0)
 print(res1)
