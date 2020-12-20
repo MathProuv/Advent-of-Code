3c671e3b6d69b05df566c05b91b80fd7151134b5
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2020-12-20 18:26:50 +0100

    day20 part1 qui compte les coins

--- y2020/2020-day20.py
+++ y2020/2020-day20.py
@@ -1,6 +1,7 @@
 import inputAoC as aoc
 import my_utils
 import re
+from math import prod
 
 tiles = aoc.get_input_file(20, 2020).split("\n\n")
 
@@ -16,7 +17,28 @@ tile_ex = """Tile 2797:
 #......#..
 ...######."""
 
-def get_borders(tile):
+def flip(tile):
+    pixels = tile.splitlines()
+    title = pixels.pop(0)
+    res_pixels = []
+    for line in pixels:
+        res_pixels.append(line[::-1])
+    res = [title]
+    res.extend(res_pixels)
+    return "\n".join(res)
+
+def rotate180(tile):
+    pixels = tile.splitlines()
+    title = pixels.pop(0)
+    res_pixels = []
+    for line in pixels[::-1]:
+        res_pixels.append(line[::-1])
+    res = [title]
+    res.extend(res_pixels)
+    return "\n".join(res)
+
+
+def summary(tile: str) -> dict:
     pixels = tile.splitlines()
     title = pixels.pop(0)
     number = re.search(r"Tile (\d+):", title).groups()[0]
@@ -30,11 +52,54 @@ def get_borders(tile):
     res["gauche"] = gauche
     res["droite"] = droite
     return res
+#my_utils.print_dict(summary(tile_ex))
 
-my_utils.print_dict(get_borders(tile_ex))
+def get_borders(tile: str) -> set:
+    sommaire = summary(tile)
+    res = []
+    for field, val in sommaire.items():
+        if field != "number":
+            res.append(val)
+    return res
 
+def bords(tiles):
+    frontieres = []
+    res = []
+    for tile in tiles:
+        bordures = get_borders(tile)
+        for bord in bordures:
+            frontieres.append(bord)
+            frontieres.append(bord[::-1])
+    for tile in tiles:
+        sommaire = summary(tile)
+        for field in ["haut", "bas", "gauche", "droite"]:
+            if frontieres.count(sommaire[field]) == 1:
+                res.append(int(sommaire["number"]))
+                break
+    print(len(res))
+    return res
 
-res1 = 0
+def coins(tiles):
+    frontieres = []
+    res = []
+    for tile in tiles:
+        bordures = get_borders(tile)
+        for bord in bordures:
+            frontieres.append(bord)
+            frontieres.append(bord[::-1])
+    for tile in tiles:
+        sommaire = summary(tile)
+        compt = 0
+        for field in ["haut", "bas", "gauche", "droite"]:
+            if frontieres.count(sommaire[field]) == 1:
+                compt += 1
+        if compt == 2:
+            res.append(int(sommaire["number"]))
+    print(len(res))
+    return res
+
+bords(tiles)
+res1 = prod(coins(tiles))
 print(res1)
 
 res2 = 0
