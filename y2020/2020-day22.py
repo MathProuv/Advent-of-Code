3b2e3aafc58fd397a1aa8daccb8e3c8d47f39c52
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2020-12-22 13:38:57 +0100

    day22 part1 ok

--- y2020/2020-day22.py
+++ y2020/2020-day22.py
@@ -6,6 +6,7 @@ deck1 = [int(card) for card in initial_decks[0].splitlines()[1:]]
 deck2 = [int(card) for card in initial_decks[1].splitlines()[1:]]
 
 class Deck:
+    """Objet qui représente une p(f)ile de cartes"""
     def __init__(self, initial_deck: [int]):
         self.deck = deque(initial_deck)
     
@@ -31,14 +32,15 @@ class Deck:
     def __repr__(self):
         return str(self.deck)
 
-def test1():
+def test_score():
     ex = Deck([3, 2, 10, 6, 8, 5, 9, 4, 7, 1])
     assert ex.score() == 306
     ex2 = Deck([7, 5, 6, 2, 4, 1, 10, 8, 9, 3])
     assert ex2.score() == 291
-# test1()
+# test_score()
 
 class Combat:
+    """Objet qui consiste en un jeu de Combat itératif"""
     def __init__(self, deck1, deck2):
         self.deck1 = Deck(deck1) if type(deck1) != Deck else deck1
         self.deck2 = Deck(deck2) if type(deck2) != Deck else deck1
@@ -49,14 +51,18 @@ class Combat:
             card2 = self.deck2.play()
             gagnant = self.deck1 if card1 > card2 else self.deck2
             gagnant.win(card1, card2)
-        self.resultat()
+        return self.resultat()
     
     def resultat(self):
         assert not self.deck1.is_playable() or not self.deck2.is_playable()
         gagnant = self.deck1 if not self.deck2.is_playable() else self.deck2
-        print("Le jeu gagnant est :", gagnant)
-        print("Le score est :", gagnant.score())
+        # print("Le jeu gagnant est :", gagnant)
+        score = gagnant.score()
+        # print("Le score est :", score)
+        return score
 
 
 this_game = Combat(deck1, deck2)
-this_game.game()
+res1 = this_game.game()
+print(res1)
+
