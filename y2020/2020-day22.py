5432c3d47f63334b16eb51cf1e4807275e03f50b
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2020-12-22 13:45:00 +0100

    day22 part1 ok

--- y2020/2020-day22.py
+++ y2020/2020-day22.py
@@ -7,7 +7,7 @@ deck2 = [int(card) for card in initial_decks[1].splitlines()[1:]]
 
 class Deck:
     """Objet qui représente une p(f)ile de cartes"""
-    def __init__(self, initial_deck: [int]) -> Deck:
+    def __init__(self, initial_deck: [int]):
         self.deck = deque(initial_deck)
     
     def play(self) -> int:
