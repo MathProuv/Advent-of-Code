2f2d49466cb4d9daa6b79280e8e60578b7c5ca6f
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2020-12-22 13:34:22 +0100

    day22 part1 ok en Poo

---
+++ y2020/2020-day22.py
@@ -0,0 +1,62 @@
+import inputAoC as aoc
+from collections import deque
+
+initial_decks = aoc.get_input_file(22, 2020).split("\n\n")
+deck1 = [int(card) for card in initial_decks[0].splitlines()[1:]]
+deck2 = [int(card) for card in initial_decks[1].splitlines()[1:]]
+
+class Deck:
+    def __init__(self, initial_deck: [int]):
+        self.deck = deque(initial_deck)
+    
+    def play(self) -> int:
+        assert self.is_playable()
+        return self.deck.popleft()
+    
+    def is_playable(self) -> bool:
+        return len(self.deck) > 0
+
+    def win(self, card1, card2) -> None:
+        # on veut card1 > card2
+        if card1 < card2:
+            card1, card2 = card2, card1
+        self.deck.append(card1)
+        self.deck.append(card2)
+    
+    def score(self, indice=1, init=0) -> int:
+        if len(self.deck) == 0: return init
+        last_card = self.deck.pop()
+        return self.score(indice + 1, init + last_card * indice)
+    
+    def __repr__(self):
+        return str(self.deck)
+
+def test1():
+    ex = Deck([3, 2, 10, 6, 8, 5, 9, 4, 7, 1])
+    assert ex.score() == 306
+    ex2 = Deck([7, 5, 6, 2, 4, 1, 10, 8, 9, 3])
+    assert ex2.score() == 291
+# test1()
+
+class Combat:
+    def __init__(self, deck1, deck2):
+        self.deck1 = Deck(deck1) if type(deck1) != Deck else deck1
+        self.deck2 = Deck(deck2) if type(deck2) != Deck else deck1
+    
+    def game(self):
+        while self.deck1.is_playable() and self.deck2.is_playable():
+            card1 = self.deck1.play()
+            card2 = self.deck2.play()
+            gagnant = self.deck1 if card1 > card2 else self.deck2
+            gagnant.win(card1, card2)
+        self.resultat()
+    
+    def resultat(self):
+        assert not self.deck1.is_playable() or not self.deck2.is_playable()
+        gagnant = self.deck1 if not self.deck2.is_playable() else self.deck2
+        print("Le jeu gagnant est :", gagnant)
+        print("Le score est :", gagnant.score())
+
+
+this_game = Combat(deck1, deck2)
+this_game.game()
