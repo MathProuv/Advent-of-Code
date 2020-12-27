181f7feaff4218ae57ba931a8913a6b5ce6d1c97
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2020-12-27 22:13:44 +0100

    agrandir_grid déplacé dans my_utils

--- my_utils.py
+++ my_utils.py
@@ -20,3 +20,14 @@ def bin_n(number: int, n: int) -> str:
     res = bin(number)[2:]
     res = '0'*(n-len(res)) + res
     return res
+
+def agrandir_grid(grid, n=1):
+    res = [0] * (len(grid) + 2*n)
+    for z in range(len(res)):
+        res[z] = [0] * (len(grid[0]) + 2*n)
+        for y in range(len(res[z])):
+            if n <= z < len(grid)+n and n <= y < len(grid[z-n])+n:
+                res[z][y] = '.' * n + grid[z-n][y-n] + '.' * n
+            else:
+                res[z][y] = '.' * (len(grid[0][0]) + 2*n)
+    return res
