9bcf679fc732362315689623318a80ab75c38309
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2020-12-28 12:57:55 +0100

    day1 au format des other days

--- y2020/2020-day01.py
+++ y2020/2020-day01.py
@@ -1,14 +1,16 @@
 import inputAoC
 
-file = inputAoC.get_input(1, 2020)
+file = inputAoC.get_input_file(1, 2020)
 
-x = list(map(int, file.split("\n")))
+x = list(map(int, file.splitlines()))
+# x = [int(nb) for nb in file.splitlines()]
 
 for nb1 in x:
     nb2 = 2020-nb1
     if nb2 in x:
-        print(nb1*nb2)
+        res1 = nb1*nb2
         break
+print(res1)
 
 for i in range(len(x)):
     nb1 = x[i]
@@ -17,4 +19,6 @@ for i in range(len(x)):
         nb2 = x[j]
         nb3 = objectif - nb2
         if nb3 in x[j+1:]:
-            print(nb1*nb2*nb3) # nb1, nb2, nb3
+            res2 = nb1*nb2*nb3 # nb1, nb2, nb3
+            break
+print(res2)
