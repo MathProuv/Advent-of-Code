742fe2decc8000f1d0d4e22958b5c708c1db1d34
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2021-12-07 12:20:45 -0500

    init

--- y2017/2017-day01.py
+++ y2017/2017-day01.py
@@ -1,5 +1,6 @@
 import inputAoC as aoc
 
-input = aoc.get_input_file(1,2017)
+input = aoc.get_input_file(1,2017,True)
+print(input)
 
 print("pas encore résolu")
---
+++ y2021/2021-day08.py
@@ -0,0 +1,6 @@
+import inputAoC as aoc
+
+input = """"""
+#input = aoc.get_input_file(8,2021,True)
+
+print(input)
