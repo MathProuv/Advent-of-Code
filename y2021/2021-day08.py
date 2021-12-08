7654659a257a2a638ff5da498210e0fee46891cd
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2021-12-08 00:20:57 -0500

    day 8 part 1

--- y2021/2021-day08.py
+++ y2021/2021-day08.py
@@ -1,6 +1,26 @@
 import inputAoC as aoc
 
-input = """"""
-#input = aoc.get_input_file(8,2021)
+input = """acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"""
+input = aoc.get_input_file(8,2021)
+entries = input.splitlines()
 
-print(input)
+for i in range(len(entries)):
+    entry = entries[i]
+    patterns, values = entry.split(" | ")
+    values = values.split(" ")
+    entries[i] = (patterns, values)
+
+#print(entries)
+
+nbs = [0] * 10
+
+for entry in entries:
+    values = entry[1]
+    for val in values:
+        if len(val) == 2: nbs[1] += 1
+        elif len(val) == 3: nbs[7] += 1
+        elif len(val) == 4: nbs[4] += 1
+        elif len(val) == 7: nbs[8] += 1
+
+res1 = nbs[1] + nbs[4] + nbs[7] + nbs[8]
+print(res1)
