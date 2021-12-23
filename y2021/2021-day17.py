1b6ab320cf5b8858e38ca7035153d153981ab6fd
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2021-12-23 00:12:07 -0500

    day 17

--- y2021/2021-day17.py
+++ y2021/2021-day17.py
@@ -1,7 +1,8 @@
 import inputAoC as aoc
+from math import sqrt
 
 input = "target area: x=20..30, y=-10..-5"
-#input = aoc.get_input_file(17,2021)
+input = aoc.get_input_file(17,2021)
 #target area: x=155..215, y=-132..-72
 
 def get_target(input):
@@ -19,7 +20,7 @@ def is_in_target(x,y, target):
     return xs[0] <= x <= xs[1] and ys[0] <= y <= ys[1]
 
 def launch(vx,vy,target):
-    """returns (has_reached, trajectory)"""
+    """returns a boolean indicating if it has reached"""
     assert(not is_in_target(0,0,target)) #else not borned
     x,y = 0,0
     trajectory = [(x,y)]
@@ -28,8 +29,30 @@ def launch(vx,vy,target):
         y += vy
         vx -= 1 if vx > 0 else -1 if vx < 0 else 0
         vy -= 1
-        trajectory.append((x,y))
-    print(trajectory)
+        #trajectory.append((x,y))
+    #print(trajectory)
     return is_in_target(x,y, target)
 
-print(launch(6,9, target))
+
+#print(launch(6,9, target))
+#print(launch(29,-6, target))
+
+My = target[1][0]
+res1 = My*(My+1)//2
+print(res1)
+
+def test_interval(target):
+    mx = 0
+    Mx = target[0][1]
+    my = target[1][0]
+    My = -my
+    compt = 0
+    for vx in range(mx,Mx+1):
+        for vy in range(my,My+1):
+            if launch(vx,vy,target):
+                #print(vx,vy)
+                compt += 1
+    return compt
+
+res2 = test_interval(target)
+print(res2)
