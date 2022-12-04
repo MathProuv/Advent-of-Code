35585d87803a3ddb073b02b666a974cb31c9951e
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2022-12-04 14:37:02 -0500

    day 4 with regex

--- y2022/2022-day04.py
+++ y2022/2022-day04.py
@@ -1,4 +1,5 @@
 import inputAoC as aoc
+import re
 
 input = """2-4,6-8
 2-3,4-5
@@ -7,13 +8,12 @@ input = """2-4,6-8
 6-6,4-6
 2-6,4-8"""
 input = aoc.get_input_file(4,2022)
-pairs = [pair.split(',') for pair in input.split('\n')]
+pairs = input.split('\n')
 
 res1 = 0
 res2 = 0
-for i1, i2 in pairs:
-    left1, right1 = map(int, i1.split('-'))
-    left2, right2 = map(int, i2.split('-'))
+for pair in pairs:
+    left1, right1, left2, right2 = map(int,re.match("([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)", pair).groups())
     if left2 <= left1 and right1 <= right2 or left1 <= left2 and right2 <= right1:
         res1 += 1
     #if not(right1 < left2 or right2 < left1):
