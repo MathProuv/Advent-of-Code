ee39cdc27044ad1745eb8411b03568e684fb12f7
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2022-12-05 05:49:41 -0500

    day 5 refractor

--- y2022/2022-day05.py
+++ y2022/2022-day05.py
@@ -11,32 +11,32 @@ move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2"""
-n = 3
-input, n = aoc.get_input_file(5,2022), 9
-stacks_int, instrs = input.split("\n\n")
+input = aoc.get_input_file(5,2022)
+stacks_input, instrs = input.split("\n\n")
+stacks_input = stacks_input.splitlines()
+n = int(re.findall('\d+',stacks_input[-1])[-1])
+# n = 9 # 3 in example
 
 stacks = dict()
-for i in range(1,n+1):
-    stacks[i] = deque()
-for line in stacks_int.split('\n')[:-1][::-1]:
-    for i in range(n):
-        item = line[4*i+1]
-        if item != " ":
-            stacks[i+1].append(item)
+for i in range(n):
+    stacks[i+1] = deque()
+    for line in stacks_input[:-1][::-1]:
+        crate = line[4*i+1]
+        if crate != " ": stacks[i+1].append(crate)
 
-stacks9001 = dict()
+stacks2 = dict()
 for i in range(1,n+1):
-    stacks9001[i] = stacks[i].copy()
+    stacks2[i] = stacks[i].copy()
 
 for instr in instrs.splitlines():
     nb, from_stack, to_stack = map(int,re.match("move (\d+) from (\d) to (\d)",instr).groups())
     to_move = []
     for _ in range(nb):
         stacks[to_stack].append(stacks[from_stack].pop())
-        to_move.append(stacks9001[from_stack].pop())
-    stacks9001[to_stack].extend(to_move[::-1])
+        to_move.append(stacks2[from_stack].pop())
+    stacks2[to_stack].extend(to_move[::-1])
 
-def res(stacks):
+def top_crates(stacks):
     ans = ""
     for i in range(1,n+1):
         last = stacks[i].pop()
@@ -44,7 +44,7 @@ def res(stacks):
         stacks[i].append(last)
     return ans
 
-res1 = res(stacks)
+res1 = top_crates(stacks)
 print(res1)
-res2 = res(stacks9001)
+res2 = top_crates(stacks2)
 print(res2)
