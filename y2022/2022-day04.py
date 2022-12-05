6f9cb0e4410bbe8e22e8699485f0e2c7c3957dc6
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2022-12-04 20:40:24 -0500

    day 4 with different wways to read numbers in a txt file

--- y2022/2022-day04.py
+++ y2022/2022-day04.py
@@ -13,9 +13,19 @@ pairs = input.split('\n')
 res1 = 0
 res2 = 0
 for pair in pairs:
-    left1, right1, left2, right2 = map(int,re.match("([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)", pair).groups())
+    # two splits
+    i1, i2 = pair.split(',')
+    left1, right1 = map(int, i1.split('-'))
+    left2, right2 = map(int, i2.split('-'))
+    # one split
+    left1, right1, left2, right2 = map(int, pair.replace(",", "-").split("-"))
+    # regex match find all digits
+    left1, right1, left2, right2 = map(int, re.findall("[0-9]+", pair))
+    # regex match exactly
+    left1, right1, left2, right2 = map(int, re.match("(\d+)-(\d+),(\d+)-(\d+)", pair).groups())
+
     res1 += left2 <= left1 and right1 <= right2 or left1 <= left2 and right2 <= right1
     res2 += left2 <= right1 and left1 <= right2
-    #not(right1 < left2 or right2 < left1)
+    #       not(right1 < left2 or right2 < left1)
 print(res1)
 print(res2)
