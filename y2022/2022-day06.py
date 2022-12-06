c8b090a28667c36617e7a8d0fdb38cbb6816dcab
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2022-12-06 00:27:01 -0500

    day 6

---
+++ y2022/2022-day06.py
@@ -0,0 +1,14 @@
+import inputAoC as aoc
+
+input = """nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"""
+input = aoc.get_input_file(6,2022)
+
+n = len(input)
+
+def marker(input, n):
+    for i in range(len(input)):
+        if len(set(input[i:i+n])) == n: return i+n
+    raise TypeError("no marker of size "+n+" found")
+
+res1, res2 = marker(input,4), marker(input,14)
+print(res1,res2)
