71e2894708e56fae6044c97cd8541f8e41d79021
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2022-12-10 00:58:41 -0500

    day 10

---
+++ y2022/2022-day10.py
@@ -0,0 +1,31 @@
+import inputAoC as aoc
+
+input = open("../inputs/2022_10_example.txt").read()
+input = aoc.get_input_file(10,2022)
+
+instrs = input.splitlines()
+
+x = 1
+xs = []
+for instr in instrs:
+    if instr == "noop":
+        xs += [x]
+    else:
+        xs += [x,x]
+        x += int(instr[5:])
+
+res1 = 0
+for i in range(20,222,40):
+    res1 += i * xs[i-1]
+print(res1)
+
+res2 = ""
+for c in range(240):
+    if abs(xs[c] - c%40) <= 1: res2 += "#"
+    else: res2 += "."
+
+for i in range(0,202,40):
+    print(res2[i:i+40])
+
+res2 = "EALGULPG" #read the console
+print(res1, res2)
