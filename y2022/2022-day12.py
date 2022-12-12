4651202804096ae08f4a67e957c01891bc1e0db1
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2022-12-12 02:53:53 -0500

    day 12 refractored

--- y2022/2022-day12.py
+++ y2022/2022-day12.py
@@ -9,18 +9,18 @@ input = aoc.get_input_file(12,2022)
 
 start = input.index('S')
 end = input.index('E')
-grid = [list(map(lambda x:ord(x)-ord('a'), line)) for line in input.splitlines()]
+n, m = input.count('\n')+1, input.index('\n')
+input = input.replace('S','a').replace('E','z')
+input = input.splitlines()
+grid = [list(map(lambda x:ord(x)-ord('a'), line)) for line in input]
 
-n,m = len(grid),len(grid[0])
 start = (start//(m+1),start%(m+1))
 end = (end//(m+1),end%(m+1))
-grid[start[0]][start[1]] = 0
-grid[end[0]][end[1]] = 25
 
 ds = [[-1 for _ in range(m)] for _ in range(n)]
 ds[end[0]][end[1]] = 0
 
-def get_neighbours(i,j):
+def get_neighbours(i,j,n=n,m=m):
     ans = set()
     if i>0: ans.add((i-1,j))
     if i<n-1: ans.add((i+1,j))
@@ -39,9 +39,7 @@ while change:
         neighs = get_neighbours(x,y)
         for i,j in neighs.intersection(checked):
             if grid[i][j] - grid[x][y] <= 1:
-                if ds[x][y] < 0: 
-                    ds[x][y] = ds[i][j] + 1
-                    change = True
+                if ds[x][y] < 0: ds[x][y], change = ds[i][j] + 1, True
                 else: ds[x][y] = min(ds[x][y],ds[i][j] + 1)
                 checked.add((x,y))
                 for to_add in get_neighbours(x,y).difference(checked):
@@ -51,9 +49,9 @@ while change:
 res1 = ds[start[0]][start[1]]
 print(res1)
 
-potential_starts = [(i,j) for i in range(n) for j in range(m) if grid[i][j] == 0]
+starts = [(i,j) for i in range(n) for j in range(m) if grid[i][j] == 0]
 res2 = res1
-for i,j in potential_starts:
+for i,j in starts:
     if ds[i][j] >= 0:
         res2 = min(res2, ds[i][j])
 
