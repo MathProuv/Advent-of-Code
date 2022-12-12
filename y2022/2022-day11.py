59f29d706bed993fd7961fcb4bd6c5602165d52c
Mathilde Prouvost <mathilde.prouvost@gmail.com>
2022-12-11 19:46:08 -0500

    day 11 with res function and both results

--- y2022/2022-day11.py
+++ y2022/2022-day11.py
@@ -3,51 +3,41 @@ import re
 
 data = aoc.get_input_file(11,2022)
 
-regex = """Monkey \d+:
+regex = """Monkey (\d+):
   Starting items: (\d+[, \d+]*)
-  Operation: new = old (\*|\+) (old|\d+)
+  Operation: new = ([old \*\+\d]+)
   Test: divisible by (\d+)
     If true: throw to monkey (\d+)
     If false: throw to monkey (\d+)"""
 class Monkey:
     def __init__(self,data) -> None:
         args = re.match(regex,data).groups()
-        self.items = list(map(int, args[0].split(', ')))
-        op, d = args[1], args[2]
-        if d == "old": self.operation = lambda old:old**2
-        elif op == "*": self.operation = lambda old:old*int(d)
-        else: self.operation = lambda old:old+int(d)
-        prime, if_true, if_false = int(args[3]), int(args[4]), int(args[5])
-        self.test = lambda new: if_false if new%prime else if_true
-        self.nb_inspections = 0
+        self.name = int(args[0])
+        self.items = list(map(int, args[1].split(', ')))
+        self.operation = eval("lambda old:" + args[2])
+        self.prime, if_true, if_false = int(args[3]), int(args[4]), int(args[5])
+        self.test = lambda new: if_false if new % self.prime else if_true
 
-monkeys = []
-for data_monkey in data.split('\n\n'):
-    monkeys.append(Monkey(data_monkey))
-
-n = len(monkeys)
-operations = [monkey.operation for monkey in monkeys]
-tests = [monkey.test for monkey in monkeys]
-items = [monkey.items for monkey in monkeys]
-nb_inspections = [monkey.nb_inspections for monkey in monkeys]
-
-primes = [2,3,5,7,11,13,17,19]
+monkeys = [Monkey(data_monkey) for data_monkey in data.split('\n\n')]
+primes = [monkey.prime for monkey in monkeys]
 prod = 1
 for p in primes: prod *= p
-def round():
-    for monkey in range(n):
-        for item in items[monkey]:
-            new = operations[monkey](item)
-            new %= prod
-            n_monkey = tests[monkey](new)
-            items[n_monkey].append(new)
-        nb_inspections[monkey] += len(items[monkey])
-        items[monkey].clear()
 
-for r in range(10000):
-    if not r%1000: print("round",r)
-    round()
+def res(nb,func, monkeys=monkeys):
+    items = [monkey.items.copy() for monkey in monkeys]
+    nb_inspections = [0 for _ in monkeys]
+    for r in range(nb):
+        #if not r%1000: print("round",r,"of",nb)
+        for monkey in monkeys:
+            for item in items[monkey.name]:
+                new = monkey.operation(item)
+                new = func(new)
+                n_monkey = monkey.test(new)
+                items[n_monkey].append(new)
+            nb_inspections[monkey.name] += len(items[monkey.name])
+            items[monkey.name].clear()
+    nb_inspections = sorted(nb_inspections)
+    return nb_inspections[-1] * nb_inspections[-2]
 
-nb_inspections_sorted = sorted(nb_inspections)
-res1 = nb_inspections_sorted[-1] * nb_inspections_sorted[-2]
-print(res1)
+print(res1 := res(20,lambda n:n//3))
+print(res2 := res(10000,lambda n:n%prod))
